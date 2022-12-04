928b2e43f208069577914c1fa62e446c1ecd2b2c
rgazizulin174 <rgazizulin174@gmail.com>
2022-12-05 01:27:23 +0500

    fix password user

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 
 from rest_framework import serializers
 
@@ -11,13 +12,21 @@ class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password_repeat = serializers.CharField(write_only=True)
 
-    def create(self, validated_data):
-        password = validated_data.get('password')
-        password_repeat = validated_data.pop('password')
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_repeat = attrs.pop('password_repeat')
+
+        try:
+            validate_password(password)
+        except Exception as e:
+            raise serializers.ValidationError({'password': e.messages})
 
         if password != password_repeat:
-            raise serializers.ValidationError('Passwords do not match')
+            raise serializers.ValidationError('Password do not match')
+        return attrs
 
+    def create(self, validated_data):
+        password = validated_data.get('password')
         validated_data['password'] = make_password(password)
         instance = super().create(validated_data)
         return instance
