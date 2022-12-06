bbc7f0a992016c48e78be922a0c8107773a8cb0e
rgazizulin174 <rgazizulin174@gmail.com>
2022-12-06 20:04:15 +0500

    add verification VK

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     #  third-party apps
     'rest_framework',
     'corsheaders',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -144,4 +145,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 CORS_ALLOW_ALL_ORIGINS = True
+
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+SOCIAL_AUTH_VK_OAUTH2_KEY = env.str('SOCIAL_AUTH_VK_OAUTH2_KEY')
+SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str('SOCIAL_AUTH_VK_OAUTH2_SECRET')
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/logged-in/'
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
