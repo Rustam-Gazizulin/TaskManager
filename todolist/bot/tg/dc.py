0a50bec2876f5246e2360c9f7a3b64f3b326d3b2
rgazizulin174 <rgazizulin174@gmail.com>
2022-12-25 19:38:31 +0500

    add app bot and model1

---
+++ todolist/bot/tg/dc.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class GetUpdatesResponse:
+    ok: bool
+    result: List[UpdateObj]
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class SendMessageResponse:
+    ok: bool
+    result: Message
+
+    class Meta:
+        unknown = EXCLUDE
