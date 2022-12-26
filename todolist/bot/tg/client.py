a0a843ca46b07155a96d5bf3777f4f6b8d70c757
rgazizulin174 <rgazizulin174@gmail.com>
2022-12-26 22:44:06 +0500

    add marsmellow schema and dataclass

---
+++ todolist/bot/tg/client.py
@@ -0,0 +1,25 @@
+import requests
+
+from bot.tg import dc
+
+
+class TgClient:
+    def __init__(self, token):
+        self.token = token
+
+    def get_url(self, method: str):
+        return f"https://api.telegram.org/bot{self.token}/{method}"
+
+    def get_updates(self, offset: int=0, timeout: int=60) -> dc.GetUpdatesResponse:
+        url = self.get_url('getUpdates')
+        response = requests.get(url, params={"offset": offset, "timeout": timeout})
+        return dc.GET_UPDATES_SCHEMA.load(response.json())
+
+    def send_message(self, chat_id: int, text: str) -> dc.SendMessageResponse:
+        url = self.get_url('sendMessage')
+        response = requests.get(url, params={"chat_id": chat_id, "text": text})
+        return dc.SEND_MESSAGE_RESPONSE_SCHEMA.load(response.json())
+
+cl = TgClient("5713673670:AAGlsWfGCfrVj-DEM4AJjxYFJJQJib1iAAc")
+print(cl.get_updates(offset=0, timeout=60))
+# print(cl.send_message(chat_id=896458874, text='good'))
--- todolist/bot/tg/dc.py
+++ todolist/bot/tg/dc.py
@@ -1,6 +1,52 @@
-from dataclasses import dataclass
+from dataclasses import field
 from typing import List
 
+import marshmallow_dataclass
+from marshmallow_dataclass import dataclass
+from marshmallow import EXCLUDE
+
+@dataclass
+class MessageFrom:
+    id: int
+    is_bot: bool
+    first_name: str | None
+    last_name: str | None
+    username: str
+    
+    class Meta:
+        unknown = EXCLUDE
+
+@dataclass
+class MessageChat:
+    id: int
+    first_name: str | None
+    last_name: str | None
+    username: str | None
+    type: str
+    title: str | None
+    
+    class Meta:
+        unknown = EXCLUDE
+
+@dataclass
+class Message:
+    message_id: int
+    msg_from: MessageFrom = field(metadata={'data_key': 'from'})
+    chat: MessageChat
+    date: int
+    text: str | None
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+@dataclass
+class UpdateObj:
+    update_id: int
+    message: Message
+
+    class Meta:
+        unknown = EXCLUDE
 
 @dataclass
 class GetUpdatesResponse:
@@ -18,3 +64,6 @@ class SendMessageResponse:
 
     class Meta:
         unknown = EXCLUDE
+
+GET_UPDATES_SCHEMA = marshmallow_dataclass.class_schema(GetUpdatesResponse)()
+SEND_MESSAGE_RESPONSE_SCHEMA = marshmallow_dataclass.class_schema(SendMessageResponse)()
